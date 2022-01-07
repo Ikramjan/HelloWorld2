c2ae0a6b3f9cd9f5581a15afa7f14a10316cdc33
calculator <ddd>
2022-01-07 20:52:47 +0600

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Икрамджан Абдукадыров on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
